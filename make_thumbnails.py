46a706a3621fedc13e27e8682a2de076f14189bf
apiersa <ap@mobostudio.com>
2016-03-31 12:28:41 +0200

    Save thumbnails in new folder
    ...

--- make_thumbnails.py
+++ make_thumbnails.py
@@ -1,10 +1,27 @@
+####
+ #
+#  GOOD VIBES DEVELOPMENT :)
+ #
+####
 from PIL import Image
 import glob, os
 
 size = 400, 400
 
-for infile in glob.glob("*.jpg"):
+directory = os.path.dirname( os.path.realpath(__file__) ) + "\\thumb"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+for infile in glob.glob("*"):
 	file, ext = os.path.splitext(infile)
-	im = Image.open(infile)
-	im.thumbnail(size)
-	im.save(file + ".thumbnail.jpg", "JPEG")
+	try:
+		im = Image.open(infile)
+		im.thumbnail(size)
+		im.save(directory + "\\" + file + ".thumbnail.jpg", "JPEG")
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)	
+	
+	except:
+		print "Unexpected error..."
+		raise	
